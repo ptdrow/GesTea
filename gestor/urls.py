bbbbed22af81acd455a6f544c044b026b66f2db4
ptdrow <pedrodvf@gmail.com>
2020-01-13 19:27:47 -0300

    Añadiendo página de datos del contacto

--- gestor/urls.py
+++ gestor/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
    path('', views.homepage, name='homepage'),
    path('clientes/', views.shop_list, name='shop_list'),
+   path('contact/<int:pk>/', views.contact_details, name='contact_details'),
 
 ]
--- gestor/views.py
+++ gestor/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Cliente
+from .models import Contacto
 
 def homepage(request):
    return render(request, 'homepage.html', {})
@@ -7,5 +8,9 @@ def homepage(request):
 def shop_list(request):
    shops = Cliente.objects.all().order_by('barrio')
    return render(request, 'shops.html', {'shops': shops})
+
+def contact_details(request, pk):
+   contact = get_object_or_404(Contacto, pk=pk)
+   return render(request, 'contact.html', {'contact': contact})
 
 
