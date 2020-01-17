16f36fc54f11d6bb454841e30d6bcfdbe7ea77d1
ptdrow <pedrodvf@gmail.com>
2020-01-17 19:59:36 -0300

    Cambiando el funcionamiento del JS para los droplist de los productos. (Tiene bugs)

--- gestor/forms.py
+++ gestor/forms.py
@@ -6,18 +6,20 @@ class PedidosForm(forms.Form):
    coleccion = forms.ModelChoiceField(label="Colección",queryset=Coleccion.objects.all(),
 widget = forms.Select(attrs = {'onchange' : "myFunction();"}))
    #producto = forms.ModelChoiceField(label="Producto",queryset=Producto.objects.all())
-   forms.ModelChoiceField(label="Producto",queryset=Producto.objects.all())
-   presentacion = forms.ModelChoiceField(label="Presentación", queryset=Presentacion.objects.all())
-   cantidad = forms.IntegerField(label="Cantidad",min_value=1)
+   
    
    def __init__(self, *args, **kwargs):
         super(PedidosForm, self).__init__(*args, **kwargs)
         colecciones_ids = Coleccion.objects.all().values_list('id',flat=True)
+
         for pk in colecciones_ids:
             this_queryset = Coleccion.objects.get(id=pk).productos.all()
             self.fields['coleccion' + str(pk)] = forms.ModelChoiceField(label="Producto",
                                                                         queryset=this_queryset,
                                                                         disabled=True)
-            self.fields['coleccion' + str(pk)].widget.attrs.update({'class': 'special'})
+            self.fields['coleccion' + str(pk)].widget.attrs.update({'class': 'coleccion_drop'})
+
+        self.fields['presentacion'] = forms.ModelChoiceField(label="Presentación", queryset=Presentacion.objects.all())
+        self.fields['cantidad'] = forms.IntegerField(label="Cantidad",min_value=1)
 
 
